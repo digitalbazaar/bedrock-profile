a4e7d6384f4c6da3c92c6f762f4e913e0731bddb
Ganesh Annan <gannan@digitalbazaar.com>
2020-02-21 15:05:07 -0500

    Add initial files.

---
+++ test/test.config.js
@@ -0,0 +1,43 @@
+
+/*
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {config} = require('bedrock');
+const path = require('path');
+
+config.mocha.tests.push(path.join(__dirname, 'mocha'));
+const {permissions, roles} = config.permission;
+
+// MongoDB
+config.mongodb.name = 'bedrock_profile_test';
+config.mongodb.dropCollections = {};
+config.mongodb.dropCollections.onInit = true;
+config.mongodb.dropCollections.collections = [];
+
+// HTTPS Agent
+config['https-agent'].rejectUnauthorized = false;
+
+// KMS
+config.kms.allowedHost = config.server.host;
+
+// Account
+roles['bedrock-test.regular'] = {
+  id: 'bedrock-test.regular',
+  label: 'Account Test Role',
+  comment: 'Role for Test User',
+  sysPermission: [
+    permissions.ACCOUNT_ACCESS.id,
+    permissions.ACCOUNT_UPDATE.id,
+    permissions.ACCOUNT_INSERT.id,
+    permissions.EDV_CONFIG_ACCESS.id,
+    permissions.EDV_CONFIG_UPDATE.id,
+    permissions.EDV_CONFIG_REMOVE.id
+  ]
+};
+
+// KMS HTTPS API
+// optionally require an authenticated session
+// this option may be set to false when operating behind an authenticated proxy
+config['kms-http'].requireAuthentication = false;
---
+++ test/test.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+const bedrock = require('bedrock');
+require('bedrock-account');
+require('bedrock-express');
+require('bedrock-edv-storage');
+require('bedrock-https-agent');
+require('bedrock-jsonld-document-loader');
+require('bedrock-kms');
+require('bedrock-kms-http');
+require('bedrock-passport');
+require('bedrock-permission');
+require('bedrock-profile');
+require('bedrock-server');
+require('bedrock-ssm-mongodb');
+require('bedrock-test');
+bedrock.start();
