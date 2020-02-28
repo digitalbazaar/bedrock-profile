7af43439e92bf96fa245758e4f385cf9aefc158d
Ganesh Annan <gannan@digitalbazaar.com>
2020-02-28 14:12:35 -0500

    Finish the initial implementation.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -112,7 +112,7 @@ describe('bedrock-profile', () => {
             }
           });
           ({profileAgent: fetchedProfileAgent} =
-              await profileAgents.getByProfile({profile}));
+              await profileAgents.getByProfile({profile, account}));
         } catch(e) {
           error = e;
         }
@@ -128,6 +128,46 @@ describe('bedrock-profile', () => {
           fetchedProfileAgent.capabilityInvocationKey);
       });
     }); // end get a profile agent
+    describe('Get All Profile Agents', () => {
+      it('successfully gets all profile agents by "account"', async () => {
+        const account = uuid();
+        let error;
+        let profileAgent0;
+        let profileAgent1;
+        let profileAgent2;
+        let fetchedProfileAgents;
+        try {
+          const createThreeProfileAgents = [0, 1, 2].map(async () => {
+            return profileAgents.create({account});
+          });
+          [
+            {profileAgent: profileAgent0},
+            {profileAgent: profileAgent1},
+            {profileAgent: profileAgent2}
+          ] = await Promise.all(createThreeProfileAgents);
+          fetchedProfileAgents = await profileAgents.getAll({account});
+        } catch(e) {
+          error = e;
+        }
+        assertNoError(error);
+        should.exist(profileAgent0);
+        should.exist(profileAgent1);
+        should.exist(profileAgent2);
+        fetchedProfileAgents.should.be.an('array');
+        fetchedProfileAgents.length.should.equal(3);
+        fetchedProfileAgents[0].profileAgent.account.should.equal(account);
+        fetchedProfileAgents[1].profileAgent.account.should.equal(account);
+        fetchedProfileAgents[2].profileAgent.account.should.equal(account);
+        const [fetchedProfileAgent0] = fetchedProfileAgents.filter(
+          ({profileAgent}) => profileAgent.id === profileAgent0.id);
+        should.exist(fetchedProfileAgent0);
+        fetchedProfileAgent0.profileAgent.id.should.equal(profileAgent0.id);
+        fetchedProfileAgent0.profileAgent.keystore.should.equal(
+          profileAgent0.keystore);
+        fetchedProfileAgent0.profileAgent.capabilityInvocationKey.should.equal(
+          profileAgent0.capabilityInvocationKey);
+      });
+    }); // end get all profile agents
     describe('Update Profile Agent', () => {
       it('successfully update a profile agent', async () => {
         const account = uuid();
@@ -193,21 +233,85 @@ describe('bedrock-profile', () => {
   describe('profiles API', () => {
     describe('Create Profile', () => {
       it('successfully create a profile', async () => {
+        const account = uuid();
+        const settings = {name: 'Example Profile', color: '#ff0000'};
         let error;
-        let result;
+        let profile;
         try {
-          const settings = {name: 'Example Profile', color: '#ff0000'};
-          result = await profiles.create({account: uuid(), settings});
+          profile = await profiles.create({account, settings});
         } catch(e) {
           error = e;
         }
         assertNoError(error);
-        should.exist(result);
+        should.exist(profile);
+        profile.id.should.be.a('string');
+        profile.name.should.equal(settings.name);
+        profile.color.should.equal(profile.color);
       });
     }); // end create a profile agent
     describe('Get Profile', () => {
-      it.skip('successfully get a profile', async () => {
+      it('successfully get a profile', async () => {
+        const account = uuid();
+        const settings = {name: 'Example Profile', color: '#ff0000'};
+        let error;
+        let profile;
+        let fetchedProfile;
+        try {
+          profile = await profiles.create({account, settings});
+          const {id: profileId} = profile;
+          fetchedProfile = await profiles.get({profileId, account});
+        } catch(e) {
+          error = e;
+        }
+        assertNoError(error);
+        should.exist(profile);
+        should.exist(fetchedProfile);
+        fetchedProfile.name.should.equal(settings.name);
+        fetchedProfile.color.should.equal(settings.color);
+        profile.id.should.equal(fetchedProfile.id);
       });
-    }); // end create a profile agent
-  }); // end profileAgents API
+    }); // end get a profile
+    describe('Get All Profiles', () => {
+      it('successfully get all profiles by "account"', async () => {
+        const account = uuid();
+        const settings = {name: 'Example Profile', color: '#ff0000'};
+        let error;
+        let profile0;
+        let profile1;
+        let profile2;
+        let fetchedProfiles;
+        try {
+          const create3Profiles = [0, 1, 2].map(async i => {
+            return profiles.create({
+              account,
+              settings: {
+                ...settings,
+                name: settings.name + i
+              }
+            });
+          });
+          [profile0, profile1, profile2] = await Promise.all(create3Profiles);
+          fetchedProfiles = await profiles.getAll({account});
+        } catch(e) {
+          error = e;
+        }
+        assertNoError(error);
+        should.exist(profile0);
+        should.exist(profile1);
+        should.exist(profile2);
+        should.exist(fetchedProfiles);
+        fetchedProfiles.should.be.an('array');
+        fetchedProfiles.length.should.equal(3);
+        fetchedProfiles[0].type.should.equal('Profile');
+        fetchedProfiles[0].name.should.equal(settings.name + '0');
+        fetchedProfiles[0].color.should.equal(settings.color);
+        fetchedProfiles[1].type.should.equal('Profile');
+        fetchedProfiles[1].name.should.equal(settings.name + '1');
+        fetchedProfiles[1].color.should.equal(settings.color);
+        fetchedProfiles[2].type.should.equal('Profile');
+        fetchedProfiles[2].name.should.equal(settings.name + '2');
+        fetchedProfiles[2].color.should.equal(settings.color);
+      });
+    }); // end get a profile
+  }); // end profiles API
 }); // end bedrock-profile
