747e8e3b932b5b0689527308409373eea04f8869
Ganesh Annan <gannan@digitalbazaar.com>
2020-02-29 13:39:33 -0500

    Update API to use id in function signature for clarity.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -59,29 +59,29 @@ describe('bedrock-profile', () => {
   describe('profileAgents API', () => {
     describe('Create Profile Agent', () => {
       it('successfully create a profile agent', async () => {
-        const account = uuid();
+        const accountId = uuid();
         let error;
         let profileAgent;
         try {
-          ({profileAgent} = await profileAgents.create({account}));
+          ({profileAgent} = await profileAgents.create({accountId}));
         } catch(e) {
           error = e;
         }
         assertNoError(error);
         should.exist(profileAgent);
-        profileAgent.account.should.equal(account);
+        profileAgent.account.should.equal(accountId);
         profileAgent.sequence.should.equal(0);
 
       });
     }); // end create a profile agent
     describe('Get Profile Agent', () => {
       it('successfully get a profile agent by "id"', async () => {
-        const account = uuid();
+        const accountId = uuid();
         let error;
         let profileAgent;
         let fetchedProfileAgent;
         try {
-          ({profileAgent} = await profileAgents.create({account}));
+          ({profileAgent} = await profileAgents.create({accountId}));
           const {id} = profileAgent;
           ({profileAgent: fetchedProfileAgent} = await profileAgents.get({id}));
         } catch(e) {
@@ -96,23 +96,23 @@ describe('bedrock-profile', () => {
         profileAgent.capabilityInvocationKey.should.equal(
           fetchedProfileAgent.capabilityInvocationKey);
       });
-      it('successfully get a profile agent by "profile"', async () => {
-        const account = uuid();
-        const profile = `did:example:${uuid()}`;
+      it('successfully get a profile agent by "profileId"', async () => {
+        const accountId = uuid();
+        const profileId = `did:example:${uuid()}`;
         let error;
         let profileAgent;
         let fetchedProfileAgent;
         try {
-          ({profileAgent} = await profileAgents.create({account}));
+          ({profileAgent} = await profileAgents.create({accountId}));
           await profileAgents.update({
             profileAgent: {
               ...profileAgent,
               sequence: profileAgent.sequence + 1,
-              profile
+              profile: profileId
             }
           });
           ({profileAgent: fetchedProfileAgent} =
-              await profileAgents.getByProfile({profile, account}));
+              await profileAgents.getByProfile({profileId, accountId}));
         } catch(e) {
           error = e;
         }
@@ -122,30 +122,30 @@ describe('bedrock-profile', () => {
         profileAgent.id.should.equal(fetchedProfileAgent.id);
         profileAgent.sequence.should.equal(0);
         fetchedProfileAgent.sequence.should.equal(1);
-        fetchedProfileAgent.profile.should.equal(profile);
+        fetchedProfileAgent.profile.should.equal(profileId);
         profileAgent.keystore.should.equal(fetchedProfileAgent.keystore);
         profileAgent.capabilityInvocationKey.should.equal(
           fetchedProfileAgent.capabilityInvocationKey);
       });
     }); // end get a profile agent
     describe('Get All Profile Agents', () => {
-      it('successfully gets all profile agents by "account"', async () => {
-        const account = uuid();
+      it('successfully gets all profile agents by "accountId"', async () => {
+        const accountId = uuid();
         let error;
         let profileAgent0;
         let profileAgent1;
         let profileAgent2;
         let fetchedProfileAgents;
         try {
-          const createThreeProfileAgents = [0, 1, 2].map(async () => {
-            return profileAgents.create({account});
+          const create3ProfileAgents = [0, 1, 2].map(async () => {
+            return profileAgents.create({accountId});
           });
           [
             {profileAgent: profileAgent0},
             {profileAgent: profileAgent1},
             {profileAgent: profileAgent2}
-          ] = await Promise.all(createThreeProfileAgents);
-          fetchedProfileAgents = await profileAgents.getAll({account});
+          ] = await Promise.all(create3ProfileAgents);
+          fetchedProfileAgents = await profileAgents.getAll({accountId});
         } catch(e) {
           error = e;
         }
@@ -155,9 +155,9 @@ describe('bedrock-profile', () => {
         should.exist(profileAgent2);
         fetchedProfileAgents.should.be.an('array');
         fetchedProfileAgents.length.should.equal(3);
-        fetchedProfileAgents[0].profileAgent.account.should.equal(account);
-        fetchedProfileAgents[1].profileAgent.account.should.equal(account);
-        fetchedProfileAgents[2].profileAgent.account.should.equal(account);
+        fetchedProfileAgents[0].profileAgent.account.should.equal(accountId);
+        fetchedProfileAgents[1].profileAgent.account.should.equal(accountId);
+        fetchedProfileAgents[2].profileAgent.account.should.equal(accountId);
         const [fetchedProfileAgent0] = fetchedProfileAgents.filter(
           ({profileAgent}) => profileAgent.id === profileAgent0.id);
         should.exist(fetchedProfileAgent0);
@@ -170,21 +170,21 @@ describe('bedrock-profile', () => {
     }); // end get all profile agents
     describe('Update Profile Agent', () => {
       it('successfully update a profile agent', async () => {
-        const account = uuid();
-        const newAccount = uuid();
-        const profile = `did:example:${uuid()}`;
+        const accountId = uuid();
+        const newAccountId = uuid();
+        const profileId = `did:example:${uuid()}`;
         let error;
         let profileAgent;
         let updatedProfileAgent;
         try {
-          ({profileAgent} = await profileAgents.create({account}));
+          ({profileAgent} = await profileAgents.create({accountId}));
           const {id} = profileAgent;
           await profileAgents.update({
             profileAgent: {
               ...profileAgent,
               sequence: profileAgent.sequence + 1,
-              profile,
-              account: newAccount
+              profile: profileId,
+              account: newAccountId
             }
           });
           ({profileAgent: updatedProfileAgent} = await profileAgents.get({id}));
@@ -200,20 +200,20 @@ describe('bedrock-profile', () => {
         profileAgent.keystore.should.equal(updatedProfileAgent.keystore);
         profileAgent.capabilityInvocationKey.should.equal(
           updatedProfileAgent.capabilityInvocationKey);
-        updatedProfileAgent.profile.should.equal(profile);
-        profileAgent.account.should.equal(account);
-        updatedProfileAgent.account.should.equal(newAccount);
+        updatedProfileAgent.profile.should.equal(profileId);
+        profileAgent.account.should.equal(accountId);
+        updatedProfileAgent.account.should.equal(newAccountId);
       });
     }); // end get a profile agent
     describe('Delegate zCaps from a Profile Agent', () => {
       it('successfully delegate capabilites from a profile agent', async () => {
-        const account = uuid();
+        const accountId = uuid();
         const controller = `did:example:${uuid()}`;
         let error;
         let profileAgent;
         let delegatedZcaps;
         try {
-          ({profileAgent} = await profileAgents.create({account}));
+          ({profileAgent} = await profileAgents.create({accountId}));
           const {id: profileAgentId} = profileAgent;
           const capabilities = mockData.zcaps;
           delegatedZcaps = await profileAgents.delegateCapabilities(
@@ -223,7 +223,7 @@ describe('bedrock-profile', () => {
         }
         assertNoError(error);
         should.exist(profileAgent);
-        profileAgent.account.should.equal(account);
+        profileAgent.account.should.equal(accountId);
         profileAgent.sequence.should.equal(0);
         delegatedZcaps.length.should.equal(3);
         delegatedZcaps[0].proof.capabilityChain.length.should.equal(2);
@@ -233,12 +233,12 @@ describe('bedrock-profile', () => {
   describe('profiles API', () => {
     describe('Create Profile', () => {
       it('successfully create a profile', async () => {
-        const account = uuid();
+        const accountId = uuid();
         const settings = {name: 'Example Profile', color: '#ff0000'};
         let error;
         let profile;
         try {
-          profile = await profiles.create({account, settings});
+          profile = await profiles.create({accountId, settings});
         } catch(e) {
           error = e;
         }
@@ -251,15 +251,15 @@ describe('bedrock-profile', () => {
     }); // end create a profile agent
     describe('Get Profile', () => {
       it('successfully get a profile', async () => {
-        const account = uuid();
+        const accountId = uuid();
         const settings = {name: 'Example Profile', color: '#ff0000'};
         let error;
         let profile;
         let fetchedProfile;
         try {
-          profile = await profiles.create({account, settings});
+          profile = await profiles.create({accountId, settings});
           const {id: profileId} = profile;
-          fetchedProfile = await profiles.get({profileId, account});
+          fetchedProfile = await profiles.get({profileId, accountId});
         } catch(e) {
           error = e;
         }
@@ -272,8 +272,8 @@ describe('bedrock-profile', () => {
       });
     }); // end get a profile
     describe('Get All Profiles', () => {
-      it('successfully get all profiles by "account"', async () => {
-        const account = uuid();
+      it('successfully get all profiles by "accountId"', async () => {
+        const accountId = uuid();
         const settings = {name: 'Example Profile', color: '#ff0000'};
         let error;
         let profile0;
@@ -283,7 +283,7 @@ describe('bedrock-profile', () => {
         try {
           const create3Profiles = [0, 1, 2].map(async i => {
             return profiles.create({
-              account,
+              accountId,
               settings: {
                 ...settings,
                 name: settings.name + i
@@ -291,7 +291,7 @@ describe('bedrock-profile', () => {
             });
           });
           [profile0, profile1, profile2] = await Promise.all(create3Profiles);
-          fetchedProfiles = await profiles.getAll({account});
+          fetchedProfiles = await profiles.getAll({accountId});
         } catch(e) {
           error = e;
         }
