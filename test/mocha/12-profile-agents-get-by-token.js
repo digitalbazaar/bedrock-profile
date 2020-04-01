c347df28ce2db3c37a33e9f80e4f56661fd8df28
Matthew Collier <mcollier@digitalbazaar.com>
2020-04-01 16:28:15 -0400

    Add test for getByToken API.

---
+++ test/mocha/12-profile-agents-get-by-token.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {profileAgents} = require('bedrock-profile');
+const helpers = require('./helpers');
+const {util: {uuid}} = require('bedrock');
+const mockData = require('./mock.data');
+
+describe('profileAgents getByToken API', () => {
+  // mock session authentication for delegations endpoint
+  let passportStub;
+  before(async () => {
+    await helpers.prepareDatabase(mockData);
+    passportStub = await helpers.stubPassport();
+  });
+  after(() => {
+    passportStub.restore();
+  });
+  it('successfully gets a profile agent by token', async () => {
+    const accountId = uuid();
+    const token = uuid();
+    let error;
+    let profileAgent;
+    try {
+      ({profileAgent} = await profileAgents.create({accountId, token}));
+    } catch(e) {
+      error = e;
+    }
+    assertNoError(error);
+
+    error = null;
+    profileAgent = null;
+    try {
+      ({profileAgent} = await profileAgents.getByToken({token}));
+    } catch(e) {
+      error = e;
+    }
+    assertNoError(error);
+    should.exist(profileAgent);
+    profileAgent.account.should.equal(accountId);
+    profileAgent.sequence.should.equal(0);
+    profileAgent.token.should.equal(token);
+  });
+}); // end profileAgents getByToken API
