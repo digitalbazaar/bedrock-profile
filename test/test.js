fe2870ac11c0cfdb1964022a31365d2cdbd12042
Dave Longley <dlongley@digitalbazaar.com>
2021-07-26 14:24:32 -0400

    Update deps.

--- test/test.js
+++ test/test.js
@@ -9,10 +9,29 @@ require('bedrock-https-agent');
 require('bedrock-jsonld-document-loader');
 require('bedrock-kms');
 require('bedrock-kms-http');
+require('bedrock-meter');
+require('bedrock-meter-usage-reporter');
+const {handlers} = require('bedrock-meter-http');
 require('bedrock-passport');
 require('bedrock-permission');
 require('bedrock-profile');
 require('bedrock-server');
 require('bedrock-ssm-mongodb');
 require('bedrock-test');
+
+bedrock.events.on('bedrock.init', async () => {
+  /* Handlers need to be added before `bedrock.start` is called. These are
+  no-op handlers to enable meter usage without restriction */
+  handlers.setCreateHandler({
+    handler({meter} = {}) {
+      // use configured meter usage reporter as service ID for tests
+      meter.serviceId = bedrock.config['meter-usage-reporter'].client.id;
+      return {meter};
+    }
+  });
+  handlers.setUpdateHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setRemoveHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setUseHandler({handler: ({meter} = {}) => ({meter})});
+});
+
 bedrock.start();
