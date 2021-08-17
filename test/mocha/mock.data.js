e2cdcf7b80045356d22da2c9c8cc7ecf5a6e85a8
Dave Longley <dlongley@digitalbazaar.com>
2021-08-17 11:27:26 -0400

    Use bedrock-meter-usage-report 2.x.

--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -11,6 +11,16 @@ const {constants: {ZCAP_CONTEXT_URL}} = require('@digitalbazaar/zcapld');
 const data = {};
 module.exports = data;
 
+// mock product IDs and reverse lookup for webkms/edv/etc service products
+data.productIdMap = new Map([
+  // webkms service
+  ['webkms', 'urn:uuid:80a82316-e8c2-11eb-9570-10bf48838a41'],
+  ['urn:uuid:80a82316-e8c2-11eb-9570-10bf48838a41', 'webkms'],
+  // edv service
+  ['edv', 'urn:uuid:dbd15f08-ff67-11eb-893b-10bf48838a41'],
+  ['urn:uuid:dbd15f08-ff67-11eb-893b-10bf48838a41', 'edv']
+]);
+
 const accounts = data.accounts = {};
 const zcaps = data.zcaps = [];
 
