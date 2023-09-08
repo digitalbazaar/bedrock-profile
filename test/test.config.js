cb142c6f00187e60fcc4fb9170fe97bb9702d601
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-09-08 14:45:48 -0400

    Set `meter.addMockMeters` config to false in test.

--- test/test.config.js
+++ test/test.config.js
@@ -31,3 +31,5 @@ config.profile.kms.baseUrl = `${config.server.baseUri}/kms`;
 config.profile.kms.ipAllowList = ['127.0.0.1/32', '::1/128'];
 
 config['did-io'].methodOverrides.v1.disableFetch = true;
+
+config.meter.addMockMeters = false;
