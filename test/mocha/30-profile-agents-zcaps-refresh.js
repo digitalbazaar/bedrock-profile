e28e3c704c6646367a6d622acff0df342f7e7d4b
Dave Longley <dlongley@digitalbazaar.com>
2023-09-25 12:21:28 -0400

    Add test to ensure in-sync user EDV doc zcaps aren't refreshed.

--- test/mocha/30-profile-agents-zcaps-refresh.js
+++ test/mocha/30-profile-agents-zcaps-refresh.js
@@ -649,6 +649,271 @@ describe('Refresh Profile Agent Zcaps', () => {
         }
       }
     });
+    it('should ensure only profile agent record zcaps are updated if user ' +
+      'EDV doc zcaps are fresh enough', async () => {
+      /* Note: When "profileAgents.getAll()" is called multiple times
+      concurrently, just one call should succeed at performing the refresh
+      while the others should return the properly refreshed records and ensure
+      that the user document sequence is only incremented once -- IF -- the
+      zcaps therein are not already fresh, i.e., have been delegated within
+      the configured `maxSyncDelta` of NOW. This test ensures that the user
+      EDV doc zcaps are NOT updated again because they are already fresh. */
+      const accountId = uuid();
+      const didMethod = 'key';
+      // add additional `credentials` and `inbox` EDVs
+      const newEdvOptions = {
+        profile: {
+          ...edvOptions.profile,
+          additionalEdvs: [
+            {referenceId: 'credentials'},
+            {referenceId: 'inbox'}
+          ]
+        }
+      };
+      const profile = await profiles.create({
+        accountId, didMethod, edvOptions: newEdvOptions, keystoreOptions
+      });
+      profile.id.should.be.a('string');
+      profile.edvs.should.be.an('object');
+      profile.edvs.should.include.keys(['user', 'credentials', 'inbox']);
+      // should get all profile agents by accountId
+      const agents = await getAllProfileAgents({
+        accountId, includeSecrets: true
+      });
+      agents.should.have.length(1);
+      const [a] = agents;
+      a.should.have.property('meta');
+      a.meta.should.have.keys(['created', 'updated']);
+      a.should.have.property('profileAgent');
+      a.profileAgent.should.have.keys([
+        'id', 'sequence', 'account', 'profile', 'controller', 'keystore',
+        'capabilityInvocationKey', 'zcaps'
+      ]);
+      a.profileAgent.sequence.should.equal(1);
+      a.profileAgent.controller.should.be.a('string');
+      const {zcaps} = a.profileAgent;
+      zcaps.should.have.keys([
+        'profileCapabilityInvocationKey', 'userDocument', 'user-edv-kak'
+      ]);
+
+      // write documents to additional EDVs (to be read later):
+      const additionalId1 = await EdvClient.generateId();
+      const additionalId2 = await EdvClient.generateId();
+      {
+        const [profileAgentRecord] = agents;
+
+        // get writable EDV document
+        const writableEdvDoc = await getProfileAgentWritableEdvDocument(
+          {profileAgentRecord, id: additionalId1, edvName: 'credentials'});
+        const newDoc = {
+          id: additionalId1,
+          content: {
+            id: 'urn:uuid:credential1',
+            name: 'credential1'
+          }
+        };
+        await writableEdvDoc.write({doc: newDoc});
+        // read written doc
+        const readDoc = await writableEdvDoc.read();
+        readDoc.content.should.deep.equal(newDoc.content);
+      }
+      {
+        const [profileAgentRecord] = agents;
+
+        // get writable EDV document
+        const writableEdvDoc = await getProfileAgentWritableEdvDocument(
+          {profileAgentRecord, id: additionalId2, edvName: 'inbox'});
+        const newDoc = {
+          id: additionalId2,
+          content: {
+            id: 'urn:uuid:inbox1',
+            name: 'inbox1'
+          }
+        };
+        await writableEdvDoc.write({doc: newDoc});
+        // read written doc
+        const readDoc = await writableEdvDoc.read();
+        readDoc.content.should.deep.equal(newDoc.content);
+      }
+
+      // intentionally update the expiration date of profile agent user doc
+      // zcaps and profile agent zcaps to a date 15 days from now which is less
+      // than the refresh threshold value of 1 month
+      const now = Date.now();
+      // 15 days in milliseconds
+      const expiresIn15Days = new Date(now + 15 * 24 * 60 * 60 * 1000)
+        .toISOString();
+      const profileAgentRecord = klona(agents[0]);
+      const profileSigner = await profileAgents.getProfileSigner(
+        {profileAgentRecord});
+      const zcap = zcaps.userDocument;
+      const edvId = parseEdvId({capability: zcap});
+      const edvClient = new EdvClient({id: edvId, httpsAgent});
+      const docId = zcap.invocationTarget.split('/').pop();
+      const edvConfig = await getEdvConfig({edvClient, profileSigner});
+
+      const profileAgentUserDoc = await getProfileAgentUserDoc({
+        edvClient, profileSigner, docId, edvConfig
+      });
+      profileAgentUserDoc.sequence.should.equal(0);
+      const updateProfileAgent = klona(a.profileAgent);
+
+      // update zcaps expiration for profile agent (note: will invalidate zcaps)
+      await updateZcapsExpiration({
+        profileAgent: updateProfileAgent,
+        newExpires: expiresIn15Days,
+      });
+      // Note: DO NOT update profile agent user doc zcaps. This simulates
+      // another process having updated them previously but has failed to
+      // update the profile agent record zcaps.
+
+      // get the updated profileAgent record, zcaps should be updated to expire
+      // in 15 days
+      const updatedRecord = await profileAgents.get({
+        id: updateProfileAgent.id
+      });
+      updatedRecord.profileAgent.sequence.should.equal(2);
+      const {zcaps: updatedProfileAgentZcaps} = updatedRecord.profileAgent;
+      verifyZcapsExpiration({
+        zcaps: updatedProfileAgentZcaps,
+        expectedExpires: expiresIn15Days
+      });
+
+      // get the updated profile agent user document, zcaps should NOT be
+      // updated to expire in 15 days
+      const updatedProfileAgentUserDoc = await getEdvDocument({
+        docId, edvConfig, edvClient, profileSigner
+      });
+      updatedProfileAgentUserDoc.sequence.should.equal(0);
+      const {
+        zcaps: updatedProfileAgentUserDocZcaps
+      } = updatedProfileAgentUserDoc.content;
+      // get the current year
+      const currentYear = new Date().getFullYear();
+      const expectedExpiresYear = currentYear + 1;
+      verifyZcapsExpiration({
+        zcaps: updatedProfileAgentUserDocZcaps,
+        expectedExpiresYear
+      });
+
+      // only profile agent record zcaps should be refreshed
+      // when getAll() is called
+      const promises = [];
+      for(let i = 0; i < 10; i++) {
+        const refreshedAgentsPromise = profileAgents.getAll({accountId});
+        promises.push(refreshedAgentsPromise);
+      }
+      const refreshedAgentsRecords = await Promise.all(promises);
+      // all 10 calls should return a refreshed record, but only one should
+      // have updated the record, expected the sequence to have incremented
+      // expected all calls to return records with refreshed zcaps
+      const expectedSequence = updatedRecord.profileAgent.sequence + 1;
+      refreshedAgentsRecords.forEach(records => {
+        const refreshedAgent = records[0].profileAgent;
+        refreshedAgent.sequence.should.equal(expectedSequence);
+        const {zcaps: refreshedZcaps} = refreshedAgent;
+        // zcaps expiration should have been set to a year from now
+        verifyZcapsExpiration({
+          zcaps: refreshedZcaps,
+          expectedExpiresYear
+        });
+      });
+
+      // get updated profile agent user doc zcaps
+      const refreshedProfileAgentUserDoc = await getEdvDocument({
+        docId, edvConfig, edvClient, profileSigner
+      });
+      // expected the profile agent user document sequence to have NOT
+      // incremented
+      const expectedProfileAgentUserDocSequence =
+        updatedProfileAgentUserDoc.sequence;
+      refreshedProfileAgentUserDoc.sequence.should.equal(
+        expectedProfileAgentUserDocSequence);
+      const {
+        zcaps: refreshedProfileAgentUserDocZcaps
+      } = refreshedProfileAgentUserDoc.content;
+      // zcaps expiration should be set to a year from now
+      verifyZcapsExpiration({
+        zcaps: refreshedProfileAgentUserDocZcaps,
+        expectedExpiresYear
+      });
+
+      // ensure zcaps still work
+      {
+        // get secrets
+        const [[profileAgentRecord]] = refreshedAgentsRecords;
+        const {secrets} = await profileAgents.get(
+          {id: profileAgentRecord.profileAgent.id, includeSecrets: true});
+        profileAgentRecord.secrets = secrets;
+
+        // read as profile agent and compare to doc retrieved as profile
+        const edvDoc = await getUserEdvDocument({profileAgentRecord});
+        const doc = await edvDoc.read();
+        doc.should.deep.equal(refreshedProfileAgentUserDoc);
+
+        // get writable user EDV document
+        const id = await EdvClient.generateId();
+        const writableEdvDoc = await getProfileAgentWritableEdvDocument(
+          {profileAgentRecord, id, edvName: 'user'});
+        const newDoc = {
+          id,
+          content: {
+            id: 'urn:uuid:testuser',
+            name: 'foo'
+          }
+        };
+        await writableEdvDoc.write({doc: newDoc});
+        // read written doc
+        const readDoc = await writableEdvDoc.read();
+        readDoc.content.should.deep.equal(newDoc.content);
+
+        // ensure additional EDV docs can be read and written to
+        {
+          // get writable EDV document
+          const writableEdvDoc = await getProfileAgentWritableEdvDocument(
+            {profileAgentRecord, id: additionalId1, edvName: 'credentials'});
+          const expectedDoc = {
+            id: additionalId1,
+            content: {
+              id: 'urn:uuid:credential1',
+              name: 'credential1'
+            }
+          };
+          // read previously written doc
+          const readDoc = await writableEdvDoc.read();
+          readDoc.content.should.deep.equal(expectedDoc.content);
+          // update doc
+          readDoc.content.newProperty = 'foo';
+          await writableEdvDoc.write({doc: readDoc});
+          // read doc again and compare
+          const readDoc2 = await writableEdvDoc.read();
+          readDoc2.content.should.deep.equal(readDoc.content);
+        }
+        {
+          const [profileAgentRecord] = agents;
+
+          // get writable EDV document
+          const writableEdvDoc = await getProfileAgentWritableEdvDocument(
+            {profileAgentRecord, id: additionalId2, edvName: 'inbox'});
+          const expectedDoc = {
+            id: additionalId2,
+            content: {
+              id: 'urn:uuid:inbox1',
+              name: 'inbox1'
+            }
+          };
+          // read previously written doc
+          const readDoc = await writableEdvDoc.read();
+          readDoc.content.should.deep.equal(expectedDoc.content);
+          // update doc
+          readDoc.content.newProperty = 'foo';
+          await writableEdvDoc.write({doc: readDoc});
+          // read doc again and compare
+          const readDoc2 = await writableEdvDoc.read();
+          readDoc2.content.should.deep.equal(readDoc.content);
+        }
+      }
+    });
   });
 });
 
